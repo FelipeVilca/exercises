df5ae91bffcf24ddc64516b574eb3bd6ab22c8f3
Fille <felipe.vilca87@gmail.com>
2019-09-19 14:22:35 +0200

    exercises 1 completed

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1,tal2,sum;
+            Console.WriteLine("Skriv in två heltal ");
+            Console.Write("Skriv in ditt första tal: ");
+            tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Skriv in ditt andra tal: ");
+            tal2 = Convert.ToInt32(Console.ReadLine());
+            sum = tal1+tal2;
+            Console.WriteLine("Dina tal summerat blir: " + sum);
+
+        }
+    }
+}
