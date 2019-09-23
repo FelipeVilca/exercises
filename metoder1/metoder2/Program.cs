ecd2ca70db89fc272f4febfc89445c00b2083dc0
Fille <felipe.vilca87@gmail.com>
2019-09-23 10:41:17 +0200

    Metoder2

---
+++ metoder1/metoder2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace metoder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+            fealing();
+        }
+        static void Hello()
+        {
+            Console.WriteLine("Hej på dig ");
+        }
+        static void fealing()
+        {
+            Console.WriteLine("Hur mår du? ");
+        }
+    }
+}
