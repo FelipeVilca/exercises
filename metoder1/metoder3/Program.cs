0e4f27caf2b4276e8797965ba26a97b4a23bff6b
Fille <felipe.vilca87@gmail.com>
2019-09-23 11:06:50 +0200

    Metoder3

---
+++ metoder1/metoder3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace metoder3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två heltal för addition: ");
+            Console.Write("Skriv ditt första tal ");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Skriv in ditt andra tal: ");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            summan(tal1, tal2);
+        }
+         static void summan(int tal1, int tal2)
+        {
+            int sum = tal1 + tal2;
+            Console.Write("Dina tal adderat blir: " + sum);
+        }
+    }
+}
