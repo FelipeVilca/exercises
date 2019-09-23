339769a89be07de6bba9636ce9cd3bc4b6525187
Fille <felipe.vilca87@gmail.com>
2019-09-23 10:22:43 +0200

    Metoder1

---
+++ metoder1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace metoder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            name ("Felipe Vilca ");
+        }
+        static void name(string name)
+        {
+            Console.WriteLine("Hej hur mår du " + name + "?");
+        }
+    }
+}
