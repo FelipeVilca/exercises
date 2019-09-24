122bc20c56fc31018d2503cc5d59e3b0fcdc83ba
Fille <felipe.vilca87@gmail.com>
2019-09-24 16:22:58 +0200

    Metod4

---
+++ metoder1/metoder4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace metoder4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej vad kostade din vara? ");
+            double pris = Convert.ToInt32(Console.ReadLine());
+            double moms = 1.25;
+
+            double result = medMoms(pris, moms);
+            Console.WriteLine("Din vara inklusive moms blir " + result);          
+                  
+        }
+        static double medMoms(double pris, double moms)
+        {
+            double sum = pris * moms;
+            return sum;
+        }
+    }
+}
