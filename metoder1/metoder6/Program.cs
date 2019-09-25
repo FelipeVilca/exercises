0c6f2c3bbe57715b3b0fe7320f267caeb7a97d2d
Fille <felipe.vilca87@gmail.com>
2019-09-25 11:03:34 +0200

    Metoder6

---
+++ metoder1/metoder6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace metoder6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Skapa metoden ToPercentage() som tar in ett decimaltal och returnerar ett heltal. 
+            */
+            Console.WriteLine("Vänligen skriv in ett decimaltal ");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+            double avrundat = tal1 + 0.5;
+            Console.WriteLine("Ditt tal avrundat till helta blir " + avrundat);
+        }
+    }
+}
