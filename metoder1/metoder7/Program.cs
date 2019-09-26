a2490408c70b5cbc59f08056eab7249cbff31e79
Fille <felipe.vilca87@gmail.com>
2019-09-26 13:47:52 +0200

    Metoder7

---
+++ metoder1/metoder7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace metoder7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Skapa en metod som kontrollerar om man är myndig. Metoden ska ta in 
+            ett heltal och returnera true eller false.
+            */
+            Console.WriteLine("Hej hur gammal är du? ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            bool myMyndig = true;
+            myMyndig = Age >= 18; 
+            {
+                Console.WriteLine(myMyndig);
+            }
+        } 
+        static bool myndig(int Age, bool myMyndig)
+        {
+            return myMyndig;
+        }   
+    }  
+}
