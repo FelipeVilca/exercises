ed7be0be387407736d862b98637d4fa6a47a04d5
Fille <felipe.vilca87@gmail.com>
2019-09-26 14:20:39 +0200

    Metoder5

---
+++ metoder1/metoder5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace metoder5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Fortsättning på föregående övning. Gör om funktionen så att man i
+            metodanropet (via parameterlistan) får ange vad momsen ligger på.  
+            */
+            Console.WriteLine("Hej vad kostade din vara? ");
+            double pris = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hur mycket % är momsen på? ");
+            double moms = Convert.ToInt32(Console.ReadLine());
+            double delen = moms/100;
+            double tal1 = 1;
+
+
+            double result = medMoms(pris, delen, tal1);
+            Console.WriteLine("Din vara inklusive moms blir " + result);
+        }
+        static double medMoms(double pris, double delen, double tal1)
+        {
+            double sum = pris * (delen + tal1);
+            return sum;
+        }
+    }
+}
