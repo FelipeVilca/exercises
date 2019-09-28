78e328cc212a6b1c2e875ce67e220f9ff47eafca
Fille <felipe.vilca87@gmail.com>
2019-09-28 12:15:35 +0200

    Exercises10

---
+++ exercises10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercises10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Skriv ett program som räknar baklänges från 40-30 och skriver ut dem på skärmen. 
+            */
+            for (int i = 40; i <= 30; i--) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
