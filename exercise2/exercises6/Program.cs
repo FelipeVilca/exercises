0fcd2c96f9fbeea0b7e90949fa89734de87b2dca
Fille <felipe.vilca87@gmail.com>
2019-09-28 10:41:19 +0200

    Exercises6

---
+++ exercise2/exercises6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercises6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Skapa ett program som ställer frågan ”Är det fint väder?”. 
+            Om användaren svara ”j” skriver programmet ut ”Vi går på picknick!”. 
+            Annars händer ingenting. 
+            */
+            Console.WriteLine("Är det fint väder? ");
+            string answer = Console.ReadLine();
+            if (answer == "j")
+            {
+                Console.WriteLine("Vi går på picknick ");
+            }
+        }
+    }
+}
