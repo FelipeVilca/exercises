55965fd9d2d6e232320228cc844fce207fb48f09
Fille <felipe.vilca87@gmail.com>
2019-09-28 10:47:18 +0200

    exercises6full

--- exercise2/exercises6/Program.cs
+++ exercise2/exercises6/Program.cs
@@ -10,12 +10,25 @@ namespace exercises6
             Skapa ett program som ställer frågan ”Är det fint väder?”. 
             Om användaren svara ”j” skriver programmet ut ”Vi går på picknick!”. 
             Annars händer ingenting. 
+            Arbeta vidare på övningen ovanför men lägg till att användaren kan svara ”n”. 
+            Då skriver programmet ut ”Vi stannar inne och läser en bok”. Det ska inte spela 
+            någon roll om användaren matar in stor eller liten bokstav.Kontrollera värdet (del 3)
+            Arbeta vidare på övningen. Gör så att om användaren matar in något annat 
+            än ”J”, ”j”, ”n”, ”N” så skriver programmet ut ”Jag förstår inte!”.
             */
             Console.WriteLine("Är det fint väder? ");
             string answer = Console.ReadLine();
-            if (answer == "j")
+            if (answer == "j" || answer == "J")
             {
                 Console.WriteLine("Vi går på picknick ");
+            }
+            else if (answer == "n" || answer == "N")
+            {
+                Console.WriteLine("Vi stannar inne och läser en bok ");
+            }
+            else
+            {
+                Console.WriteLine("Jag förstår inte! ");
             }
         }
     }
