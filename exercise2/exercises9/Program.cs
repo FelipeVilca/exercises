223cecbe98381d48fc5241c16753488dc78ad54d
Fille <felipe.vilca87@gmail.com>
2019-09-28 11:00:55 +0200

    Exercises9

---
+++ exercise2/exercises9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercises9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Skriv ett program som en gång loopar igenom talen 1-10 och skriver ut dem på skärmen. 
+            */
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
