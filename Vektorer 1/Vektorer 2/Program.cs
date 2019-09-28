e5136a8afeab26c91a40a43084bd2e807e299058
Fille <felipe.vilca87@gmail.com>
2019-09-28 09:58:25 +0200

    exercises3

---
+++ Vektorer 1/Vektorer 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Vektorer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Skapa en string array med fem element. Användaren får mata in fem namn
+            via en for-loop. Programmet skriver sedan ut dessa namn med hjälp av en for-loop.*/
+            string[] name = new string [5];
+            Console.WriteLine("Hej vänligen skriv in fem namn ");
+            int sum = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write("Skriv in namn: ");
+                string names = Console.ReadLine();
+                sum = sum + name[i];
+                
+            }
+            
+
+        }
+    }
+}
---
+++ exercise2/execise3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace execise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Låt användaren mata in två namn. Skriv sedan ut en kort berättelse med dessa två namn. 
+            */
+            Console.WriteLine("Hej vänligen skriv in två namn ");
+            Console.Write("Skriv in ditt första namn: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Skriv in ditt andra namn: ");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Det var en gång två personer den ena hette " + name1 + 
+            " och den andra hette " + name2 + " De var väldigt lyckliga båda två :) ");
+
+        }
+    }
+}
