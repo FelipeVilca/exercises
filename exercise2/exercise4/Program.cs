0926bc653f77f9e8cfda0d34a04b330b4c7a0532
Fille <felipe.vilca87@gmail.com>
2019-09-28 10:36:34 +0200

    Exercises5

---
+++ exercise2/exercise4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Prova att använda olika numeriska värden i UNICODE-tabellen. Vilka slutsatser kan du dra av detta? 
+            */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ exercise2/exercise5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            I denna övning ska du göra ett program som låter användaren mata in sitt namn. 
+            Därefter hälsar du användaren välkommen. Du ska sedan låta användaren mata in sin 
+            ålder och spara undan den i lämplig variabel. Räkna sedan ut antalet dagar användaren 
+            har levt utifrån det angivna åldern. Presentera detta för användaren. 
+            */
+            Console.WriteLine("Hej och välkommen ");
+            Console.Write("Vad heter du? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hej och välkommen "+ name + " hur gammal är du? ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            int year, sum;
+            year = 365;
+            sum = Age * year;
+            Console.WriteLine(name + " du har levt i " + sum + " dagar ");
+        }
+    }
+}
