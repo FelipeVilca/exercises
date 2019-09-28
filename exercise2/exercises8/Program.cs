acdd5bd260ed1817a2f151a427ba9ae8cee8f0d5
Fille <felipe.vilca87@gmail.com>
2019-09-28 10:56:03 +0200

    Exercises8

---
+++ exercise2/exercises8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace exercises8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Skriv ett program som simulerar en bank. 
+            Man ska kunna sätta in pengar och ta ut pengar, samt kolla saldo. 
+            Detta görs via en meny som du ska göra med switch-sats. 
+            Observera att du bör jobba med loopar i denna uppgift! 
+            */
+          while (true)
+            {//Skriver menyn för användaren
+                Console.WriteLine("Välj:");
+                Console.WriteLine("[I]nsättning");
+                Console.WriteLine("[U]ttag");
+                Console.WriteLine("[S]aldo");
+                Console.WriteLine("[A]vsluta");
+                string menySelection = Console.ReadLine();//Val för användaren
+                if (menySelection == "I" || menySelection == "i")
+                {
+                    Console.WriteLine("Var god och mata in sedlarna");
+                }
+                else if (menySelection == "U" || menySelection == "u")
+                {
+                    Console.WriteLine("Skriv in belopp");
+                }
+                else if (menySelection == "S" || menySelection == "s")
+                {
+                    Console.WriteLine("Saldo är 500:-");
+                }
+                else if (menySelection == "A" || menySelection == "a")
+                {
+                    Console.WriteLine("Välkommen åter");
+                }
+                {
+                    break; //Avbryter loopen
+                }
+            }
+        }
+    }
+}
